7f7cff90f38d217ff7f26475cb900a484b689914
Macsmith21 <smithmac405@gmail.com>
2024-02-02 20:05:41 -0500

    update

--- CSCE242/assignments/07/script.js
+++ CSCE242/assignments/07/script.js
@@ -1,5 +1,7 @@
 window.onload = () => {
     document.getElementById("imgSwitch").onclick = () =>{
+        let slider = document.getElementById("myRange")
+        let image = document.getElementById("RotatingImg")
         
     }
 };
