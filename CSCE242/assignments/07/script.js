4a1a0e06cf908c54a77b134d3a28a606ff981472
Macsmith21 <smithmac405@gmail.com>
2024-02-03 17:54:22 -0500

    assign 7 Complete

--- CSCE242/assignments/07/script.js
+++ CSCE242/assignments/07/script.js
@@ -2,13 +2,32 @@
 
 window.onload = () => 
 {
+    let Switch= document.getElementById("imgSwitch");
     let slider= document.getElementById("Slider");
     let image = document.getElementById("RotatingImg");
+    let starCol = document.getElementById("Star-Col");
+    
+
+    const Switcher =() =>{
+     Switch.src = "https://place-hold.it/200x200/blue/red"
+    }
 
     const Rotator=()=>
    {
         image.style.transform = "rotate(" + slider.value + "deg)";
    }
+   const AddStar = () => {
+     Star = document.createElement("section");
+     starCol.append(Star);
+     Star.classList.add("star");
+     Star.innerHTML = "&star;";
+     
+
+
+
+   }
    slider.oninput=Rotator;
+   Switch.onclick = Switcher;
+   starCol.onclick = AddStar;
 
 }
