262764773b044fd5721349ec55c0aedd206ea263
Macsmith21 <smithmac405@gmail.com>
2024-03-12 19:31:54 -0400

    update

---
+++ CSCE242/assignments/11/script.js
@@ -0,0 +1,19 @@
+class bear {
+    constructor(name, region, size, lifespan, sciname, diet, color) {
+
+      this.name = name;
+      this.region=region;
+      this.size=size;
+      this.lifespan=lifespan;
+      this.sciname=sciname;
+      this.diet=diet;
+      this.color=color;
+    }
+}
+const bearlist=[
+    new bear('American Black Bear','North America','small','21-39','Ursus Americanus', 'Berries, fish, insects', 'Black'),
+    new bear('Brown Bear','North America, Eurasia','Large','20-30','Ursus Arctos','Berries, Fish, Small Mammals','Brown'),
+    new bear('Polar Bear','Arctic','Large','20-30','Ursus Maritimus','Seal, Walrus, Narwhal','White'),
+    new bear('Panda','China','Small','15-20','Ailuropoda Melanoleuca','Bamboo, Flora, Fish, Birds','Multi-Colored'),
+    new bear('Sloth Bear','India, Sri Lanka','Medium', '20-40','Melursus Ursinus','termites, ants, mango, sugarcane','Black')
+]
